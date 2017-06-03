b4f6a814dbbe11974b861b10f79eda3e007e0c1e
Prasanth <prasanth@farabi.ae>
2017-06-03 17:30:03 +0400

    fixed the uae number issue

--- Pod/Classes/Model/VeeContact Abstract Factory/VeeContactFactory.m
+++ Pod/Classes/Model/VeeContact Abstract Factory/VeeContactFactory.m
@@ -14,16 +14,16 @@
     VeeABRecordsImporter* abRepositoryImporter = [VeeABRecordsImporter new];
     NSArray<VeeABRecord*>* veeABRecords = [abRepositoryImporter importVeeABRecordsFromAddressBook:addressBook];
     NSMutableArray* veeContacts = [NSMutableArray new];
+    NSCharacterSet *notAllowedChars = [[NSCharacterSet characterSetWithCharactersInString:@"+0123456789"] invertedSet];
     for (VeeABRecord* veeABRecord in veeABRecords){
         
         for (NSString *phoneNumber in veeABRecord.phoneNumbers){
             
-            NSCharacterSet *notAllowedChars = [[NSCharacterSet characterSetWithCharactersInString:@"+0123456789"] invertedSet];
             NSString *resultString = [[phoneNumber componentsSeparatedByCharactersInSet:notAllowedChars] componentsJoinedByString:@""];
             
             NSError *error = nil;  // ^(?:\+971|00971|971|0971|0)(?:50|51|52|55|56)\d{7}$
-            NSRegularExpression *regularExpression = [NSRegularExpression regularExpressionWithPattern:@"^(?:\\+971||971|0(0971)?)[5][0-9]{8}$" options:0 error:&error];  //
-             NSTextCheckingResult *match = [regularExpression firstMatchInString:phoneNumber options:0 range:NSMakeRange(0, [phoneNumber length])];
+            NSRegularExpression *regularExpression = [NSRegularExpression regularExpressionWithPattern:@"^(?:\\+971|971|0(0971)?)[5][0-9]{8}$" options:0 error:&error];  //
+             NSTextCheckingResult *match = [regularExpression firstMatchInString:resultString options:0 range:NSMakeRange(0, [resultString length])];
             
             if (match) {
                 veeABRecord.phoneNumber = phoneNumber;
